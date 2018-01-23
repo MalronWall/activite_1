fc3731a9fc4a2de5f3ac6a18f44cf9095e6f03c9
MalronWall <thibaut.tourte17@gmail.com>
2018-01-24 00:48:21 +0100

    [ADD][File] Ajout de l'index

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+    include_once "functions.php";
+    $name = "'Unknown'";
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Page de test</title
+</head>
+
+<body>
+
+<header>
+
+    <h1><?php Hello($name);?></h1>
+    
+</header>
